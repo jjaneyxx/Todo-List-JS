bf9de91728b8b5c3b20eddd95363ed5e56b7657b
jjaneyxx <hmyeonge@gmail.com>
2024-08-29 00:30:05 +0900

    feat: 입력한 할일을 task-board 에 보여주기

---
+++ main.js
@@ -0,0 +1,29 @@
+let taskInput = document.getElementById("task-input");
+let addButton = document.getElementById("add-button");
+let taskList = []; // 입력된 할일을 담는 배열
+
+addButton.addEventListener("click", addTask);
+
+function addTask() {
+  taskList.push(taskInput.value);
+  console.log(taskList);
+  render();
+}
+
+function render() {
+  let resultHTML = ``;
+  for (let i = 0; i < taskList.length; i++) {
+    resultHTML += `<div class="task">
+            <div>${taskList[i]}</div>
+            <div class="button-area">
+              <button>
+                <i class="fa-solid fa-check"></i>
+              </button>
+              <button>
+                <i class="fa-solid fa-trash"></i>
+              </button>
+            </div>
+          </div>`;
+  }
+  document.getElementById("task-board").innerHTML = resultHTML;
+}
