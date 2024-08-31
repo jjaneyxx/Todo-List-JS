0281cc6a108058a8d5e5c4881ab55c6b8236347e
jjaneyxx <hmyeonge@gmail.com>
2024-09-01 00:17:43 +0900

    Feat: 아이템 삭제 기능

--- main.js
+++ main.js
@@ -15,26 +15,80 @@ function addTask() {
   if (taskInput.value == "") {
     alert("할 일을 입력하세요");
   } else {
-    taskList.push(taskInput.value);
+    // 입력한 내용과 완료여부를 체크하는 내용 포함
+    let task = {
+      id: generateRandomID(),
+      taskContent: taskInput.value,
+      isComplete: false,
+      isDeleted: false,
+    };
+    console.log(task);
+    taskList.push(task);
     render();
-    taskInput.value = "";
+    taskInput.value = ""; // 입력창 비우기
   }
 }
 
 function render() {
   let resultHTML = ``;
   for (let i = 0; i < taskList.length; i++) {
-    resultHTML += `<div class="task">
-    <div>${taskList[i]}</div>
-    <div class="button-area">
-      <button>
-        <i class="fa-solid fa-check"></i>
-      </button>
-      <button>
-        <i class="fa-solid fa-trash"></i>
-      </button>
-    </div>
-  </div>`;
+    if (taskList[i].isDeleted == true) {
+      resultHTML += "";
+    } else {
+      if (taskList[i].isComplete == true) {
+        resultHTML += `<div class="task task-box-done">
+      <div class = "task-done">${taskList[i].taskContent}</div>
+      <div class="button-area">
+        <button onclick = "toggleComplete('${taskList[i].id}')">
+          <i class="fa-solid fa-check"></i>
+        </button>
+        <button onclick = "deleteTask('${taskList[i].id}')">
+          <i class="fa-solid fa-trash"></i>
+        </button>
+      </div>
+    </div>`;
+      } else {
+        resultHTML += `<div class="task">
+        <div>${taskList[i].taskContent}</div>
+        <div class="button-area">
+          <button onclick = "toggleComplete('${taskList[i].id}')">
+            <i class="fa-solid fa-check"></i>
+          </button>
+          <button onclick = "deleteTask('${taskList[i].id}')">
+            <i class="fa-solid fa-trash"></i>
+          </button>
+        </div>
+      </div>`;
+      }
+    }
   }
   taskBoard.innerHTML = resultHTML;
+}
+
+// 체크 버튼이 클릭되었을 때 실행
+function toggleComplete(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break; // for 문 종료
+    }
+  }
+  render(); // 화면에 그리기 위해 render 함수 호출
+}
+
+function deleteTask(id) {
+  console.log("삭제");
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList[i].isDeleted = true;
+      break;
+    }
+  }
+  console.log(taskList);
+  render(); // 화면에 그리기 위해 render 함수 호출
+}
+
+// task 의 id 생성
+function generateRandomID() {
+  return Math.random().toString(36).substr(2, 16);
 }
